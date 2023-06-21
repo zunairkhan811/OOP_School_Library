5f0b758e38bfea7b77be0501e9afc4784649e670
Muhammad Zunair khan <zunairkhan811@gmail.com>
2023-06-22 01:16:57 +0500

    add code for rental.rb

---
+++ rental.rb
@@ -0,0 +1,9 @@
+class Rental
+    attr_accessor :date
+    attr_reader :person, :book
+    def initialize(date,book,person)
+        @date = date
+        @book = book
+        @person = person
+    end
+end
